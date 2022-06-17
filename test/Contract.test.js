f6359032efd1fb52178143ca3f1b4a95bf1b6119
Michael Burke <michael.dburke56@gmail.com>
2022-06-16 22:10:36 -0400

    Create simple contract and test.

--- test/Contract.test.js
+++ test/Contract.test.js
@@ -3,16 +3,19 @@ const { ethers } = require('hardhat');
 
 describe('Contract', function () {
   before(async function () {
-    //this.Contract = await ethers.getContractFactory('Contract');
+    this.Contract = await ethers.getContractFactory('Contract');
   });
 
   beforeEach(async function () {
-    //this.contract = await this.Contract.deploy();
-    //await this.contract.deployed();
+    this.contract = await this.Contract.deploy();
+    await this.contract.deployed();
   });
 
-  it('test descriptor', async function () {
-    expect('2').to.equal('2');
+  it('The addOne function should increment the state variable by one.', async function () {
+    
+    await this.contract.addOne();
+
+    expect((await this.contract.stateValue()).toString()).to.equal('1');
   });
   
 });
